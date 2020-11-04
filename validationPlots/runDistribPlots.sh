473a08e71f0433b2fe288427d21675f5a7706973
Stephanie Kwan <skwan@princeton.edu>
2020-11-04 18:27:17 +0100

    Run gen level mutauh plots for VBF and ggH

--- validationPlots/runDistribPlots.sh
+++ validationPlots/runDistribPlots.sh
@@ -1,7 +1,9 @@
 
-root -l -b -q 'runDistributionPlots.C("SUSYZHToAA_AToBB_AToTauTau_M-40", \
-                                      "ZH h #rightarrow aa #rightarrow bb#tau#tau (m_{a} = 40 GeV)",\
-                                      "/eos/user/s/skkwan/signalNanoAOD/2018/SUSYZHToAA_AToBB_AToTauTau_M-40.root")'
+root -l -b -q 'runDistributionPlots.C("SUSYVBFHToAA_AToBB_AToTauTau_M-40", \
+                                      "VBF h #rightarrow aa #rightarrow bb#tau#tau (m_{a} = 40 GeV)",\
+                                      "/eos/user/s/skkwan/hToAA/genSkims/Nov-03-2020-20h40m/SUSYVBFToHToAA_AToBB_AToTauTau_M-40GenSkim.root")'
 
-
+root -l -b -q 'runDistributionPlots.C("SUSYGluGluHToAA_AToBB_AToTauTau_M-40", \
+                                      "gg h to #rightarrow aa #rightarrow bb#tau#tau (m_{a} = 40 GeV)",\
+                                      "/eos/user/s/skkwan/hToAA/genSkims/Nov-03-2020-20h40m/SUSYVBFToHToAA_AToBB_AToTauTau_M-40GenSkim.root")'
 
--- validationPlots/runDistributionPlots.C
+++ validationPlots/runDistributionPlots.C
@@ -9,7 +9,7 @@ void runDistributionPlots(TString sampleName, TString legend, TString inputDirec
   //  gROOT->ProcessLine(".L ../baseCodeForPlots/comparisonPlots.C");
  
   std::cout << sampleName << " " << legend << " " << inputDirectory << std::endl;
-  TString treePath = "Events";
+  TString treePath = "mutau_tree";
   //  TString inputDirectory  = "/eos/user/s/skkwan/signalNanoAOD/2018/SUSYVBFToHToAA_AToBB_AToTauTau_M-40/SUSYVBFToHToAA_AToBB_AToTauTau_M-40_BATCH_1_NANO.root";
   TString outputDirectory = "/eos/user/s/skkwan/hToAA/signalGenLevelPlots/2018/" + sampleName + "/"; 
   gSystem->Exec("mkdir " + outputDirectory);
@@ -17,13 +17,13 @@ void runDistributionPlots(TString sampleName, TString legend, TString inputDirec
   TString cut = "";
 
   int nBins = 50;
-  singleDistributionPlots("Muon_pt", cut, legend, treePath, inputDirectory, outputDirectory, "Muon p_{T}", nBins, 15, 150);
-  singleDistributionPlots("Muon_eta", cut, legend, treePath, inputDirectory, outputDirectory, "Muon #eta", nBins, -3, 3);
-  singleDistributionPlots("Muon_phi", cut, legend, treePath, inputDirectory, outputDirectory, "Muon #phi", nBins, -4, 4);
+  singleDistributionPlots("pt_1", cut, legend, treePath, inputDirectory, outputDirectory, "Muon p_{T}", nBins, 15, 150);
+  singleDistributionPlots("eta_1", cut, legend, treePath, inputDirectory, outputDirectory, "Muon #eta", nBins, -3, 3);
+  singleDistributionPlots("phi_1", cut, legend, treePath, inputDirectory, outputDirectory, "Muon #phi", nBins, -4, 4);
 
-  singleDistributionPlots("Tau_pt", cut, legend, treePath, inputDirectory, outputDirectory, "Tau p_{T}", nBins, 15, 150);
-  singleDistributionPlots("Tau_eta", cut, legend, treePath, inputDirectory, outputDirectory, "Tau #eta", nBins, -3, 3);
-  singleDistributionPlots("Tau_phi", cut, legend, treePath, inputDirectory, outputDirectory, "Tau #phi", nBins, -4, 4);
+  singleDistributionPlots("pt_2", cut, legend, treePath, inputDirectory, outputDirectory, "#tau_{h} p_{T}", nBins, 15, 150);
+  singleDistributionPlots("eta_2", cut, legend, treePath, inputDirectory, outputDirectory, "#tau_{h} #eta", nBins, -3, 3);
+  singleDistributionPlots("phi_2", cut, legend, treePath, inputDirectory, outputDirectory, "#tau_{h} #phi", nBins, -4, 4);
 
   
 
