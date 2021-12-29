e1666df64cd6d35357c1b866b34cde18c2250f8a
Stephanie Kwan <skwan@princeton.edu>
2021-12-29 19:56:54 +0100

    Add check for [SUCCESS] and [WARNING]

--- latexLogReports/createReport.py
+++ latexLogReports/createReport.py
@@ -10,14 +10,17 @@ parser = argparse.ArgumentParser(description='Create a cutflow report.')
 parser.add_argument("--build",
                     help="Build the resulting .tex",
                     dest='execute', action = 'store_true')
+parser.add_argument("--rootdir",
+                    help="Path to the log files", required=True)
 parser.set_defaults(execute=True)
 
 args = parser.parse_args()
 
-rootdir='/eos/user/s/skkwan/hToAA/dataMC/intermediateTuples/2018/Nov-17-2021-02h02m-intermediate-DataMC2018_JEC_JER'
 
-# Escape latex
 def escape_latex(value):
+    '''
+    Helper function to escape special characters in LaTeX.
+    '''
     return value.replace(
         '&', '\&').replace(
         '%', '\%').replace(
@@ -31,55 +34,88 @@ def escape_latex(value):
         '~', '\\textasciitilde ').replace(
         '^', '\\textasciicircum ') 
 
+def main():
 
-# Replace function
-sectionsWithSummaries = ''
+    descriptor=os.path.basename(args.rootdir)
+    sectionsWithSummaries = ''
+    samplesThatDidNotFinish = ''
+    samplesWithWarnings = ''
 
-# Loop through log files in the directory
-for subdir, dirs, files in os.walk(rootdir):
-    for f in files:
-        
-        # Only check logRun_ files
-        if 'logRun' in f:
-            logname = os.path.basename(f)   
-            # Get the sample name using regex
-            p = re.compile('logRun_(.*)')
-            sample = p.search(logname).group(1)
-            print(">>> " + sample)
-
-            # Get the contents of the file
-            with open(os.path.join(subdir, f)) as logfile:
-                # print(logfile.read())
-                # Skip the first five lines (RooFit header)
-                skimCutFlow = logfile.readlines()[5:]    # use readlines to get a list
-                skimCutFlow = '\n'.join(skimCutFlow)      # collapse the list back into one string
-                skimCutFlow = escape_latex(skimCutFlow)
-
-                # Build the section in LaTeX
-                header = '\section{%s}' % escape_latex(sample)
-                # print(header)
-
-
-                sectionsWithSummaries += header + '\n'
-                sectionsWithSummaries += skimCutFlow 
-                sectionsWithSummaries += '\n'
+    # Loop through log files in the directory
+    for subdir, dirs, files in os.walk(args.rootdir):
+        for f in files:
+            
+            # Only check logRun_ files
+            if 'logRun' in f:
+                logname = os.path.basename(f)   
+                # Get the sample name using regex
+                p = re.compile('logRun_(.*)')
+                sample = p.search(logname).group(1)
+                print(">>> " + sample)
                 
-print(sectionsWithSummaries)
+                # Get the contents of the file
+                with open(os.path.join(subdir, f)) as logfile:
+                    # print(logfile.read())
+                    # Skip the first five lines (RooFit header)
+                    skimCutFlow = logfile.readlines()[5:]    # use readlines to get a list
+                    skimCutFlow = '\n'.join(skimCutFlow)      # collapse the list back into one string
+                    skimCutFlow = escape_latex(skimCutFlow)
+                    
+                    # Build the section in LaTeX
+                    header = '\section{%s}' % escape_latex(sample)
+                    # print(header)
+                    
+                    
+                    sectionsWithSummaries += header + '\n'
+                    if 'SUCCESS' not in skimCutFlow:
+                        sectionsWithSummaries += '[ERROR:] Run did not finish!'
+                        samplesThatDidNotFinish += escape_latex(sample) + ', '
+                    if 'WARNING' in skimCutFlow:
+                        samplesWithWarnings     += escape_latex(sample) + ', '
+                    sectionsWithSummaries += skimCutFlow 
+                    sectionsWithSummaries += '\n'
+                    
+    print(sectionsWithSummaries)
 
-# Replace the dummy section in the template
-template = open("templateCutflowReport.tex", "rt")
-report   = open("report.tex",         "wt")
+    # Replace the dummy section in the template
+    template = open("templateCutflowReport.tex", "rt")
+    report   = open("report.tex",         "wt")
+                    
+    for line in template:
+        # Modify the title
+        dummy = 'Directory'
+        if dummy in line: 
+            report.write(line.replace(dummy, escape_latex(descriptor)))
+            continue
 
-for line in template:
-    dummySection = '\section{Example}'
-    if dummySection in line:
-        report.write(line.replace(dummySection, sectionsWithSummaries))
-    else:
-        report.write(line)
+        # Print the samples that did not finish                                                  
+        dummy = 'samples that did not finish'
+        if dummy in line:
+            report.write(line.replace(dummy, samplesThatDidNotFinish))
+            continue
 
-template.close()
-report.close()
+        # Print the samples that had warnings
+        dummy = 'samples that threw warnings'
+        if dummy in line:
+            report.write(line.replace(dummy, samplesWithWarnings))
+            continue
 
-# Build the report
-command = 'pdflatex report.tex'
-os.system(command)
+        # Print the log files
+        dummySection = '\section{Example}'
+        if dummySection in line:
+            report.write(line.replace(dummySection, sectionsWithSummaries))
+        else:
+            report.write(line)
+        
+            
+    template.close()
+    report.close()
+                            
+    # Build the report
+    command = 'pdflatex report.tex' 
+    os.system(command)
+    command = 'mv report.pdf %s' % (args.rootdir)
+    os.system(command)
+
+if __name__ == "__main__":
+    main()
