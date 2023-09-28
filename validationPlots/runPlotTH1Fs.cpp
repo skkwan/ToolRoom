39382ac5e18202df8d04d5d613d19f3f9b239739
Stephanie Kwan <skwan@princeton.edu>
2023-09-28 16:08:48 +0200

    m_vis not m_sv

--- validationPlots/runPlotTH1Fs.cpp
+++ validationPlots/runPlotTH1Fs.cpp
@@ -65,6 +65,30 @@ int runPlotTH1Fs()
         plotTH1F(inputName, axisLabel, additionalLabel, false, inputDirectory, outputName, false);
       }
     }
+    else {
+      // Category
+      folder = cat + "/";
+    }
+    
+    // Path to histogram in the file
+    TString inputName = TString::Format("%s%s_m_vis", folder.c_str(), sampleName.c_str());
+
+    // Label
+    TString axisLabel = "m_{vis}";
+
+    // Label text added to plot
+    TString additionalLabel = TString::Format("%s %s", label.c_str(), cat.c_str());
+
+    // Path to output .pdf file
+    TString outputName  = TString::Format("%s/%s_%s_m_vis.pdf", outputDirectory.c_str(), sampleName.c_str(), cat.c_str());
+
+    // Nominal
+    if (cat == "") {
+      outputName = TString::Format("%s/%s_m_vis.pdf", outputDirectory.c_str(), sampleName.c_str());
+    }
+
+    std::cout << outputName << std::endl;
+    plotTH1D(inputDirectory, inputName, axisLabel, additionalLabel, outputName);
   }
   return 1; 
 
