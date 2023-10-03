457dca2614e12c92b0f0d6807983a4f940587166
Stephanie Kwan <skwan@princeton.edu>
2023-10-03 17:29:33 +0200

    also add ak4 jet pt

--- validationPlots/validationPlots.cpp
+++ validationPlots/validationPlots.cpp
@@ -17,9 +17,12 @@ void validationPlots(TString sampleName, TString legend, TString inputDirectory,
 
   TString cut = "";
 
-  singleDistributionPlots("deltaR_ak4_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "#DeltaR(close-by gen jets, nearest AK4 jet)", 40, 0, 1);
-  singleDistributionPlots("pt_resolution_ak4_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "pT resolution of close-by gen jets and nearest AK4 jet", 40, -2, 3);  
+  singleDistributionPlots("deltaR_ak4_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "#DeltaR(close-by gen b's, nearest AK4 jet)", 40, 0, 1);
+  singleDistributionPlots("pt_resolution_ak4_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "pT resolution of close-by gen b's and nearest AK4 jet", 40, -2, 3);  
   singleDistributionPlots("jet_btagDeepFlavB_ak4_matched_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "DeepFlavB score of nearest AK4 jet", 40, 0, 1);  
+  singleDistributionPlots("jetPt_ak4_matched_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "p_{T} of nearest AK4 jet", 50, 0, 200);  
+  singleDistributionPlots("genPt_leadingPair", cut, sampleName, treePath, inputDirectory, outputDirectory, "Gen p_{T} of close-by gen b's", 50, 0, 200);  
+
 
   singleDistributionPlots("closest_idx2", cut, sampleName, treePath, inputDirectory, outputDirectory, "Index of third b, if within #DeltaR < 0.4", 5, -1, 3);
   singleDistributionPlots("closest_idx3", cut, sampleName, treePath, inputDirectory, outputDirectory, "Index of fourth nearby b, if within #DeltaR < 0.4", 5, -1, 3);
