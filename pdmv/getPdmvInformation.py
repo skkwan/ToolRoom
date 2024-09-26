06c50fe93e0506061cf2f4579c697664adf06e69
Stephanie Kwan <skwan@princeton.edu>
2024-09-26 10:44:08 +0200

    add comments to getPdmvInformation.py

--- pdmv/getPdmvInformation.py
+++ pdmv/getPdmvInformation.py
@@ -1,6 +1,14 @@
 # Usage
 # First do cmsenv from some CMSSW environment, and get voms proxy, and come back to this script
 # python3 getPdmvInformation.py
+#
+# This script looks up a set of prepIDs (the LHEGEN level ones) in a range in "requests_query". Note
+# that the command-line interface gives a URL to click on to verify your login, then go back to the
+# command line and hit "Enter" per the prompt.
+# This gives a .json and in it we look up the filter efficiency. The entry to check is "4" (hardcoded in filter_efficiency).
+# Next, the script looks up the number of events in the associated AOD dataset from dasgoclient, with the --json flag
+# so it outputs to a temp.json, and we look for the entry with "nevents" in it.
+# Lastly, all of this is piped to a report.txt file.
 
 from json import dumps
 import json
@@ -13,7 +21,7 @@ from rest import McM
 
 mcm = McM(id=McM.OIDC, dev=True, debug=True)
 
-# Cascade VBF
+# Cascade VBF and ggH
 requests_query = """
     SUS-RunIISummer20UL18wmLHEGEN-01071 -> SUS-RunIISummer20UL18wmLHEGEN-01090
     SUS-RunIISummer20UL18wmLHEGEN-00934 -> SUS-RunIISummer20UL18wmLHEGEN-00953
@@ -25,6 +33,7 @@ for request in range_of_requests:
 
     dataset_name=request["reqmgr_name"][1]["content"]["pdmv_dataset_name"]
 
+    # Note: "4" is hardcoded
     filter_efficiency=request["validation"]["results"]["4"][0]["filter_efficiency"]
 
     # Now look up by the dataset name 
