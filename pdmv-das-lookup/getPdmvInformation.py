03a18b1a706cbdd4c41dcaa948330124e9cf0a59
Stephanie Kwan <skwan@princeton.edu>
2024-11-22 16:01:38 +0100

    update pdmv script

--- pdmv-das-lookup/getPdmvInformation.py
+++ pdmv-das-lookup/getPdmvInformation.py
@@ -19,19 +19,62 @@ import sys
 sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
 from rest import McM
 
-mcm = McM(id=McM.OIDC, dev=True, debug=True)
+mcm = McM(id=McM.OIDC, dev=True, debug=False)
 
-# Cascade VBF and ggH
+# https://its.cern.ch/jira/browse/HIGHPRIOREQ-760 
+
+# 2018 Cascade
+#    SUS-RunIISummer20UL17wmLHEGEN-00992 -> SUS-RunIISummer20UL17wmLHEGEN-01003
+#    SUS-RunIISummer20UL17wmLHEGEN-00847 -> SUS-RunIISummer20UL17wmLHEGEN-00859
+
+# 2018 Non-Cascade
+    # SUS-RunIISummer20UL18wmLHEGEN-00913 -> SUS-RunIISummer20UL18wmLHEGEN-00925
+    # SUS-RunIISummer20UL18wmLHEGEN-00968  
+    # SUS-RunIISummer20UL18wmLHEGEN-01058  
+    # SUS-RunIISummer20UL18wmLHEGEN-01060 -> SUS-RunIISummer20UL18wmLHEGEN-01070
+
+# 2017 Cascade
+    # SUS-RunIISummer20UL17wmLHEGEN-00900
+    # SUS-RunIISummer20UL17wmLHEGEN-01004 -> SUS-RunIISummer20UL17wmLHEGEN-01018
+    # SUS-RunIISummer20UL17wmLHEGEN-01020 -> SUS-RunIISummer20UL17wmLHEGEN-01023
+    # SUS-RunIISummer20UL17wmLHEGEN-00864 -> SUS-RunIISummer20UL17wmLHEGEN-00883
+#2017 Non-Cascade
+    # SUS-RunIISummer20UL17wmLHEGEN-00847  SUS-RunIISummer20UL17wmLHEGEN-00859
+    # SUS-RunIISummer20UL17wmLHEGEN-00901  
+    # SUS-RunIISummer20UL17wmLHEGEN-00992  SUS-RunIISummer20UL17wmLHEGEN-01003
+
+# 2016 Cascade
+    # SUS-RunIISummer20UL16wmLHEGEN-00987 -> SUS-RunIISummer20UL16wmLHEGEN-01006
+    # SUS-RunIISummer20UL16wmLHEGENAPV-00890 -> SUS-RunIISummer20UL16wmLHEGENAPV-00909
+    # SUS-RunIISummer20UL16wmLHEGEN-01112 -> SUS-RunIISummer20UL16wmLHEGEN-01131
+    # SUS-RunIISummer20UL16wmLHEGENAPV-01031 -> SUS-RunIISummer20UL16wmLHEGENAPV-01050
+
+# 2016 Non-Cascade preVFP
+    # SUS-RunIISummer20UL16wmLHEGENAPV-00873 -> SUS-RunIISummer20UL16wmLHEGENAPV-00885
+    # SUS-RunIISummer20UL16wmLHEGENAPV-00924  
+    # SUS-RunIISummer20UL16wmLHEGENAPV-01018 -> SUS-RunIISummer20UL16wmLHEGENAPV-01029
+
+# 2016 Non-Cascade postVFP
+# SUS-RunIISummer20UL16wmLHEGEN-00943 -> SUS-RunIISummer20UL16wmLHEGEN-00955
+# SUS-RunIISummer20UL16wmLHEGEN-01099 -> SUS-RunIISummer20UL16wmLHEGEN-01111
+
 requests_query = """
-    SUS-RunIISummer20UL18wmLHEGEN-01071 -> SUS-RunIISummer20UL18wmLHEGEN-01090
-    SUS-RunIISummer20UL18wmLHEGEN-00934 -> SUS-RunIISummer20UL18wmLHEGEN-00953
+    SUS-RunIISummer20UL16wmLHEGEN-00943 -> SUS-RunIISummer20UL16wmLHEGEN-00955
+    SUS-RunIISummer20UL16wmLHEGEN-01099 -> SUS-RunIISummer20UL16wmLHEGEN-01111
 """
 
 range_of_requests = mcm.get_range_of_requests(requests_query)
 print("Found %s requests" % (len(range_of_requests)))
 for request in range_of_requests:
 
-    dataset_name=request["reqmgr_name"][1]["content"]["pdmv_dataset_name"]
+    # "request" is a dictionary. We need to loop through the dictionary to get the dataset name stored in "pdmv_dataset_name", because this is inconsistently in different locations
+    arrayOfDictsToCheck = request["reqmgr_name"]
+    for dictToCheck in arrayOfDictsToCheck: # e.g. request["reqmgr_name"][2]
+        if "content" in dictToCheck:
+            d = dictToCheck["content"] # it will always be in "content"
+            if "pdmv_dataset_name" in d:
+                dataset_name=d["pdmv_dataset_name"]
+                print(f">>> Found {dataset_name}")
 
     # Note: "4" is hardcoded
     filter_efficiency=request["validation"]["results"]["4"][0]["filter_efficiency"]
@@ -56,3 +99,6 @@ for request in range_of_requests:
         dataset_name_abbrev = dataset_name[:dataset_name.find('_TuneCP5')]
         dataset_name_abbrev = dataset_name_abbrev.replace(" ", "_")
         report.write(f'{dataset_name_abbrev}  & {nevents:,} & {filter_efficiency:.6f} \\\\ \n')
+
+    with open("pythonConfig.py", "a") as report:
+        report.write(f'"{dataset_name_abbrev}": {filter_efficiency:.6f},\n')
